e01591fcf458daaf3258bd57fb887da0421e2907
Ethan Ligon <ligon@berkeley.edu>
2023-01-10 18:17:43 -0800

    Try to make this work with org-publish...

--- build-site.el
+++ build-site.el
@@ -17,10 +17,10 @@
 (require 'ox-publish)
 
 ;; Customize the HTML output
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+;(setq org-html-validation-link nil            ;; Don't show validation link
+;      org-html-head-include-scripts nil       ;; Use our own scripts
+;      org-html-head-include-default-style nil ;; Use our own styles
+;      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -28,12 +28,13 @@
        (list "org-site:main"
              :recursive t
              :base-directory "./content"
-             :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./public"
+             :publishing-function org-publish-org-to-html ;'org-html-publish-to-html
+             :publishing-directory "./_site"
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
+             :body-only t               ;; Let Jekyll handle everything but the body
              :time-stamp-file nil)))    ;; Don't include time stamp in file
 
 ;; Generate the site output
